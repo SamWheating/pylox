f5f89cca395145a01cb3a1e5b14200e23643716b
Sam Wheating <sam.wheating@shopify.com>
2022-02-21 13:23:21 -0800

    Finished scanner implementation (Up to page 60)

--- pylox/scanner.py
+++ pylox/scanner.py
@@ -5,6 +5,26 @@ from typing import List, Union
 
 
 class Scanner:
+
+    keywords = {
+        "and": TokenType.AND,
+        "class": TokenType.CLASS,
+        "else": TokenType.ELSE,
+        "false": TokenType.FALSE,
+        "for": TokenType.FOR,
+        "fun": TokenType.FUN,
+        "if": TokenType.IF,
+        "nil": TokenType.NIL,
+        "or": TokenType.OR,
+        "print": TokenType.PRINT,
+        "return": TokenType.RETURN,
+        "super": TokenType.SUPER,
+        "this": TokenType.THIS,
+        "true": TokenType.TRUE,
+        "var": TokenType.VAR,
+        "while": TokenType.WHILE,
+    }
+
     def __init__(self, source):
         self.source = source
         self.tokens = []
@@ -68,10 +88,30 @@ class Scanner:
             case " " | "\t" | "\r":
                 pass
 
+            case '"':
+                self.string()
+
+            case "\n":
+                self.line += 1
+
+            case "/":
+                if self.match("/"):
+                    while self.peek() != "\n" and not self.is_at_end():
+                        self.advance()
+                else:
+                    self.add_token(TokenType.SLASH)
+
             case _:
+
+                if c.isdigit():
+                    self.number()
+
+                elif self.is_valid_identifier(c):
+                    self.identifier()
+
                 print(self.line, "Unexpected Character.")
 
-    def match(self, expected):
+    def match(self, expected) -> bool:
 
         if self.is_at_end():
             return False
@@ -80,6 +120,31 @@ class Scanner:
 
         self.current += 1
         return True
+
+    def string(self) -> None:
+        while self.peek() != '"' and not self.is_at_end():
+            if self.peek() == "\n":
+                self.line += 1
+            self.advance()
+
+        if self.is_at_end():
+            # TODO: Propagate this error back up to the Lox Class without `raise`
+            raise Exception("Unterminated String")
+
+        self.advance()
+        value = self.source[self.start + 1 : self.current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def identifier(self) -> None:
+        while self.is_valid_identifier(self.peek()):
+            self.advance()
+
+        text = self.source[self.start : self.current]
+        self.add_token(self.keywords.get(text, TokenType.IDENTIFIER))
+
+    @staticmethod
+    def is_valid_identifier(c: str):
+        return c.replace("_", "a").isalnum()
 
     def add_token(
         self, type: TokenType, literal: Union[float, str, None] = None
@@ -91,3 +156,26 @@ class Scanner:
         c = self.source[self.current]
         self.current += 1
         return c
+
+    def number(self) -> None:
+        while self.peek().isdigit():
+            self.advance()
+
+        # Checking for the fractional part
+        if self.peek() == "." and self.peek_next().isdigit():
+            self.advance()
+
+            while self.peek().isdigit():
+                self.advance()
+
+        self.add_token(TokenType.NUMBER, float(self.source[self.start : self.current]))
+
+    def peek(self) -> str:
+        if self.is_at_end():
+            return "\0"
+        return self.source[self.current]
+
+    def peek_next(self) -> str:
+        if self.current + 1 > len(self.source):
+            return "\0"
+        return self.source[self.current + 1]
--- test/test_scanner.py
+++ test/test_scanner.py
@@ -2,15 +2,15 @@ import unittest
 from pylox.scanner import Scanner
 from pylox.token_type import TokenType
 
-class TestScanner(unittest.TestCase):
 
+class TestScanner(unittest.TestCase):
     def test_scanner_simple_lexemes(self):
 
         scanner = Scanner("()")
         tokens = scanner.scan_tokens()
-        
+
         assert len(tokens) == 3
-        
+
         expected = [TokenType.LEFT_PAREN, TokenType.RIGHT_PAREN, TokenType.EOF]
         assert [t.type for t in tokens] == expected
 
@@ -18,9 +18,9 @@ class TestScanner(unittest.TestCase):
 
         scanner = Scanner("!=")
         tokens = scanner.scan_tokens()
-        
+
         assert len(tokens) == 2
-        
+
         expected = [TokenType.BANG_EQUAL, TokenType.EOF]
         assert [t.type for t in tokens] == expected
 
@@ -28,10 +28,64 @@ class TestScanner(unittest.TestCase):
 
         scanner = Scanner("!= >=")
         tokens = scanner.scan_tokens()
-        
+
         assert len(tokens) == 3
-        
+
         expected = [TokenType.BANG_EQUAL, TokenType.GREATER_EQUAL, TokenType.EOF]
         assert [t.type for t in tokens] == expected
 
-    
+    def test_scanner_strings(self):
+
+        scanner = Scanner('"This is a String"')
+        tokens = scanner.scan_tokens()
+
+        assert len(tokens) == 2
+
+        expected = [TokenType.STRING, TokenType.EOF]
+        assert [t.type for t in tokens] == expected
+
+        expected_values = ["This is a String", None]
+        assert [t.literal for t in tokens] == expected_values
+
+    def test_scanner_numbers(self):
+
+        scanner = Scanner("13 19.1")
+        tokens = scanner.scan_tokens()
+
+        assert len(tokens) == 3
+
+        expected = [TokenType.NUMBER, TokenType.NUMBER, TokenType.EOF]
+        assert [t.type for t in tokens] == expected
+
+        expected_values = [13.0, 19.1, None]
+        assert [t.literal for t in tokens] == expected_values
+
+    def test_scanner_comments(self):
+
+        scanner = Scanner("// This is just a big comment")
+        tokens = scanner.scan_tokens()
+
+        assert len(tokens) == 1
+
+        expected = [TokenType.EOF]
+        assert [t.type for t in tokens] == expected
+
+    def test_scanner_reserved_words(self):
+
+        scanner = Scanner("if true")
+        tokens = scanner.scan_tokens()
+
+        assert len(tokens) == 3
+
+        expected = [TokenType.IF, TokenType.TRUE, TokenType.EOF]
+        assert [t.type for t in tokens] == expected
+
+    def test_scanner_identifiers(self):
+
+        scanner = Scanner("if true")
+        tokens = scanner.scan_tokens()
+
+        assert len(tokens) == 3
+
+        expected = [TokenType.IF, TokenType.TRUE, TokenType.EOF]
+        assert [t.type for t in tokens] == expected
